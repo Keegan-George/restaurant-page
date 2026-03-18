faeb232ba65ae471bca371bb48ccb3d10164f1e2
Keegan-George <39040251+Keegan-George@users.noreply.github.com>
2026-03-18 18:50:51 -0400

    Create About page content and enable tab switching logic for the navigation bar

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-import {home} from "./scripts.js";
+import { home, about } from "./scripts.js";
+
+home();
+
+const homeButton = document.querySelector(".home");
+const menuButton = document.querySelector(".menu");
+const aboutButton = document.querySelector(".about");
+
+const navBar = document.querySelector("nav");
+navBar.addEventListener("click", (event) => {
+    const navButton = event.target.closest("button");
+
+    if (!navButton) { return; }
+
+    const buttonClasses = navButton.classList;
+
+
+    if (buttonClasses.contains("home")) {
+        home()
+    }
+    else if (buttonClasses.contains("about")) {
+        about();
+    }
+});
--- src/scripts.js
+++ src/scripts.js
@@ -1,7 +1,9 @@
 import donutImg from "./donut.jpg";
 
-const home = (() => {
-    const content = document.querySelector("#content");
+const content = document.querySelector("#content");
+
+function home() {
+    content.textContent = "";
 
     const hero = document.createElement("figure");
 
@@ -19,10 +21,34 @@ const home = (() => {
 
     const blurb = document.createElement("section");
     blurb.classList.add("blurb");
-    blurb.textContent = "Artisanal donuts handcrafted by highly trained pastry chefs. Savour never before tasted flavors unlike anything you have ever experienced before.";
+    blurb.textContent = `Artisanal donuts handcrafted by highly trained pastry chefs. Savour never before tasted flavors unlike 
+    anything you have ever experienced before.`;
 
     content.appendChild(hero);
     content.appendChild(blurb);
-})();
+}
 
-export { home };
+function about() {
+    content.textContent = "";
+
+    const history = document.createElement("section");
+    history.classList.add("history");
+    history.textContent = `Established in 1923, in [city], [country], [company name] has grown from
+     a quaint mom and pop shop to multiple locations across North America. With more than 50 unique flavours, 
+     and new creations being added regularly, these hand made doughnuts offer something for everyone. Come visit us! `
+    content.appendChild(history);
+
+    const storeHours = document.createElement("section");
+    storeHours.classList.add("store-hours");
+    storeHours.textContent = "Open Hours: Tuesday to Saturday: 10:00am - 6:00pm";
+    content.appendChild(storeHours);
+
+    const address = document.createElement("section");
+    address.classList.add("address");
+    address.textContent = "1234 blah blah street, City, State H13124";
+    content.appendChild(address);
+}
+
+
+
+export { home, about };
